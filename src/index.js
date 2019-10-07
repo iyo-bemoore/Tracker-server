e40d073ba60f3d756a6fa0fe981f3875d674268d
2imad <imadyoussoufi@gmail.com>
2019-10-07 18:30:44 +0200

    finish up auth

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(authRoutes);
-
 const mongoURI =
   "mongodb+srv://user:user@cluster0-rhx1q.mongodb.net/defaultUsers?retryWrites=true&w=majority";
-
 mongoose.connect(mongoURI, {
   useCreateIndex: true,
   useNewUrlParser: true,
@@ -22,11 +21,9 @@ mongoose.connection.on("connected", () => {
 mongoose.connection.on("error", e => {
   console.error("Error connecting to Mongo ", e);
 });
-
-app.get("/", (req, res) => {
-  res.send("No data to show");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email is ${req.user.email}`);
 });
-
 app.listen(3000, () => {
   console.log("Listening");
 });
